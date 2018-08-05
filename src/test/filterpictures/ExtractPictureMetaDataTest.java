749953d6f74fd11cdff926c6976d4a27149408ac
edleijnse <ed@leijnse.info>
2018-08-05 20:06:30 +0200

    local library references added (for local compilation IntelliJ), pictureContent prepared (Microsoft Computer Vision)

--- src/test/filterpictures/ExtractPictureMetaDataTest.java
+++ src/test/filterpictures/ExtractPictureMetaDataTest.java
@@ -25,6 +25,22 @@ public class ExtractPictureMetaDataTest {
         }
     }
 
+    @Test
+    public void getPictureContentTest(){
+        ExtractPictureMetaData testee = new ExtractPictureMetaData("/Volumes/MyDrive01/Lightroom/2018","/Volumes/MyDrive01/MyLightroom.csv" );
+        File myFile = new File("src/main/resources/ExportTest/famromano-100.jpg");
+        try {
+            PictureMetaData output = testee.getPictureContent(myFile);
+            XStream xStream = new XStream();
+            System.out.println(xStream.toXML(output));
+            assertEquals("Canon EOS 6D",output.getModel().get());
+            assertEquals("Canon",output.getMake().get());
+            assertEquals("2017:11:12 10:15:55",output.getDateTime().get());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Test
     public void createCSVFileTest(){
