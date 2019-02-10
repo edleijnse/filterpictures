5f918cffc52485bb992c6e6da431d31812636ff6
Ed Leijnse <ed@leijnse.info>
2019-02-10 16:27:25 +0100

    filterpictures test with onedrive added

--- src/test/filterpictures/ExtractPictureContentDataTest.java
+++ src/test/filterpictures/ExtractPictureContentDataTest.java
@@ -15,20 +15,36 @@ public class ExtractPictureContentDataTest {
     @Test
     public void getPictureContentTest() throws IOException {
         ExtractPictureContentData testee = new ExtractPictureContentData("/Volumes/MyDrive01/Lightroom/2018","/Volumes/MyDrive01/MyLightroom.csv" );
+        getPictureContentTestPicture("C:\\Users\\edlei\\OneDrive\\Testfotos\\robot.jpg");
+        getPictureContentTestPicture("C:\\Users\\edlei\\OneDrive\\Testfotos\\ed.jpg");
+        getPictureContentTestPicture("C:\\Users\\edlei\\OneDrive\\Testfotos\\mylena.jpg");
+        getPictureContentTestPicture("C:\\Users\\edlei\\OneDrive\\Testfotos\\gemaelde.jpg");
+        getPictureContentTestPicture("C:\\Users\\edlei\\OneDrive\\Testfotos\\landschaft.jpg");
+        getPictureContentTestPicture("C:\\Users\\edlei\\OneDrive\\Testfotos\\landschaft2.jpg");
+    }
+    private void getPictureContentTestPicture(String fileName) throws IOException {
+        ExtractPictureContentData testee = new ExtractPictureContentData("/Volumes/MyDrive01/Lightroom/2018","/Volumes/MyDrive01/MyLightroom.csv" );
 
 
-        File myFile = new File("src/main/resources/ExportTest/bigmountain01.jpg");
+        // File myFile = new File("src/main/resources/ExportTest/bigmountain01.jpg");
+        File myFile = new File(fileName);
         File myFileCompressed = testee.compressJpg(myFile);
         try {
-            String mySubscriptionKey = new String(Files.readAllBytes(Paths.get("/Users/edleijnse/keys/subscriptionKey1")));
+            // String mySubscriptionKey = new String(Files.readAllBytes(Paths.get("/Users/edleijnse/keys/subscriptionKey1")));
+            String mySubscriptionKey = new String(Files.readAllBytes(Paths.get("C:\\Users\\edlei\\OneDrive\\Finanzen\\Lizensen\\Microsoft\\keys\\subscriptionKey1")));
             testee.setSubstringKey(mySubscriptionKey);
             PictureMetaData output = testee.getPictureContent(myFileCompressed);
-            XStream xStream = new XStream();
-            System.out.println(xStream.toXML(output));
+            System.out.println(fileName);
+            System.out.println(output.getVISION_CONTENT());
+            System.out.println(output.getVISION_TAGS());
+            System.out.println("---------------------------------------------------------------------------------");
+            // XStream xStream = new XStream();
+            // System.out.println(xStream.toXML(output));
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     @Test
