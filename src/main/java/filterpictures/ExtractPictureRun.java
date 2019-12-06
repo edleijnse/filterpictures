b8ebf255c740872939246ffa3f753716b32f5d13
edleijnse <ed@leijnse.info>
2019-12-06 21:21:38 +0100

    Console Class added, for executing exifTool directly to update keywords, all Files transferred for Gallery

--- src/main/java/filterpictures/ExtractPictureRun.java
+++ src/main/java/filterpictures/ExtractPictureRun.java
@@ -17,7 +17,7 @@ public class ExtractPictureRun {
     }
     public ExtractPictureRun() throws IOException {
         // this.createCSVFileRun("/Volumes/MyDrive01/BilderExport/201902ExtractSmall","/Volumes/MyDrive01/BilderExport/MyLightroomVision201902ExtractSmall.csv");
-        this.createCSVFileRun("/home/edleijnse/Annalis", "/home/edleijnse/filterpicturesAnnalis20191205.csv");
+        this.createCSVFileRun("/home/edleijnse/BilderExport/Annalis/Transformationen", "/home/edleijnse/filterpicturesBilderExportAnnalisTransformationen20191206.csv");
     }
     public void createCSVFileRun(String startDir, String writeCSV) throws IOException {
         ExtractPictureMetaData testee = new ExtractPictureMetaData(startDir,writeCSV, true );
